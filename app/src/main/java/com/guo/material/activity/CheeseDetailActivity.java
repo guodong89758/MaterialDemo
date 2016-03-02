dd668dbbf46b6015f3c1069d28ba06d92d7fab32
guodong <guodong915@163.com>
2016-03-02 17:08:51 +0800

    reveal animator

--- app/src/main/java/com/guo/material/activity/CheeseDetailActivity.java
+++ app/src/main/java/com/guo/material/activity/CheeseDetailActivity.java
@@ -2,19 +2,26 @@ package com.guo.material.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.ImageView;
 
 import com.guo.material.R;
+import com.guo.material.widget.reveal.animation.SupportAnimator;
+import com.guo.material.widget.reveal.animation.ViewAnimationUtils;
 
 /**
  * Created by admin on 2016/3/1.
  */
-public class CheeseDetailActivity extends AppCompatActivity {
+public class CheeseDetailActivity extends AppCompatActivity implements ViewTreeObserver.OnPreDrawListener {
     public static final String VIEW_NAME_HEADER_IMAGE = "detail:header:image";
+    private CoordinatorLayout detail_content;
     private CollapsingToolbarLayout collapsing_toolbar;
     private Toolbar toolbar;
     private ImageView iv_cheese;
@@ -23,6 +30,7 @@ public class CheeseDetailActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheese_detail);
+        detail_content = (CoordinatorLayout) findViewById(R.id.detail_content);
         collapsing_toolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         iv_cheese = (ImageView) findViewById(R.id.iv_cheese);
@@ -30,6 +38,7 @@ public class CheeseDetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        detail_content.getViewTreeObserver().addOnPreDrawListener(this);
         int resId = getIntent().getIntExtra("image", R.drawable.cheese_1);
         iv_cheese.setImageResource(resId);
 
@@ -45,5 +54,23 @@ public class CheeseDetailActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onPreDraw() {
+        detail_content.getViewTreeObserver().removeOnPreDrawListener(this);
+        startRevealTransition();
+        return true;
+    }
+
+    public void startRevealTransition() {
+        SupportAnimator animator = ViewAnimationUtils.createCircularReveal(detail_content, detail_content.getRight(), detail_content.getBottom(), 0, hypo(detail_content.getHeight(), detail_content.getWidth()), View.LAYER_TYPE_SOFTWARE);
+        animator.setDuration(1000);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.start();
+    }
+
+    public static float hypo(float a, float b) {
+        return (float) Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
     }
 }
